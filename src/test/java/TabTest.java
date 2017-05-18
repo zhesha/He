4b66df3df9df8514df7f80a5d1eff8d3b7928a56
Alex Zherebylo <savzhe@gmail.com>
2017-05-18 19:41:54 +0300

    added test

---
+++ src/test/java/TabTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by sashaz on 18.05.17
+ */
+class TabTest {
+    @Test
+    void constructor() {
+        new Tab();
+    }
+
+    @Test
+    void main() {
+        Tab.main(new String[]{});
+    }
+}
