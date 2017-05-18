d24dc72dc6e2ca3348bcb650ca091919b11c669f
Alex Zherebylo <savzhe@gmail.com>
2017-05-18 19:20:22 +0300

    added formatter

--- src/main/java/Tab.java
+++ src/main/java/Tab.java
@@ -1,3 +1,5 @@
+import java.text.DecimalFormat;
+
 import static java.lang.Math.cos;
 
 /**
@@ -7,7 +9,7 @@ public class Tab {
     public static void main(String[] args) {
         System.out.println("Function f(x)=2cos(x)+cos(2x) have values:");
         for (float i = 0; i <= 5; i += 0.5) {
-            System.out.println("for x="+i+" values is: "+(2*cos(i)+cos(2*i)));
+            System.out.println("for x="+new DecimalFormat("0.0").format(i)+" values is: "+new DecimalFormat("0.##").format(2*cos(i)+cos(2*i)));
         }
     }
 }
